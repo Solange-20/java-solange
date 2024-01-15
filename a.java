8c282fef0d66e5bb2e2326603222b9d9bf4e9eaa
solange20 <202213204f@utea.edu.pe>
2024-01-15 15:58:19 +0100

    primer commit

---
+++ a.java
@@ -0,0 +1,13 @@
+public class a {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+
+    }try{color(blue)
+
+    }catch(
+
+    Exception e)
+    {
+    // TODO: handle exception
+}
