036a7421aecad6498589ef060796ed009ec1fe0f
solange20 <202213204f@utea.edu.pe>
2024-01-16 18:25:43 +0100

    ejercicios

---
+++ Ejercicios/Ejercicio2.java
@@ -0,0 +1,9 @@
+package Ejercicios;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Solange Daniela Caballero Sarmiento");
+        System.out.println("Av. Jose Carlos Mareategui");
+        System.out.println("970144005");
+    }
+}
---
+++ Ejercicios/Ejercicio3.java
@@ -0,0 +1,18 @@
+package Ejercicios;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        System.out.println(" Ingles   │  Español    ");
+        System.out.println("├─────────┼─────────────┤");
+        System.out.println(" computer │  ordenador");
+        System.out.println(" student  │  alumno");
+        System.out.println(" cat      │  gato");
+        System.out.println(" penguin  │  pingüino");
+        System.out.println(" machine  │  máquina ");
+        System.out.println(" nature   │  naturaleza");
+        System.out.println(" light    │  luz");
+        System.out.println(" green    │  verde ");
+        System.out.println(" book     │  libro");
+        System.out.println(" pyramid  │  pirámide");
+    }
+}
