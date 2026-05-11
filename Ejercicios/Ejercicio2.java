74fde73421d230214fead096942ee15bfd71cfa2
Solange-20 <156683706+Solange-20@users.noreply.github.com>
2026-05-11 11:05:55 -0500

    Delete Ejercicios directory

--- Ejercicios/Ejercicio2.java
+++
@@ -1,9 +0,0 @@
-package Ejercicios;
-
-public class Ejercicio2 {
-    public static void main(String[] args) {
-        System.out.println("Solange Daniela Caballero Sarmiento");
-        System.out.println("Av. Jose Carlos Mareategui");
-        System.out.println("970144005");
-    }
-}
